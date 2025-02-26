abb91c92498be5c904d9796391553c8b2e625d44
Arsh <arshagrawal3105@gmail.com>
2025-02-26 23:14:39 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2024, Ciphervault - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
+ 

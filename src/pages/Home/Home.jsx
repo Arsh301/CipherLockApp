f70ee35ab50adddda38cbdf736f63c8ab3114aab
Arsh <arshagrawal3105@gmail.com>
2025-02-26 23:21:37 +0530

    third commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import './Home.css'
+import './home.css'
 import { CoinContext } from '../../context/CoinContext'
 import { Link } from 'react-router-dom'
 
